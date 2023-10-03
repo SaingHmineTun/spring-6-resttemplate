541a6df2f66c1b55aebace01843c6bf7ca9c6383
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2023-10-03 19:16:55 +0630

    Add dependencies to use Page in rest-template

---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6resttemplate.client;
+
+import org.junit.jupiter.api.Test;
+
+public class BeerClientImplTest {
+    @Test
+    void BeerClientImpl_listBeers() {}
+}
