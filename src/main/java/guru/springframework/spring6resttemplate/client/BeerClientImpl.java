2f8873ce548a5a5385ef3ebe1504b3b0b0d69f40
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2023-10-03 19:49:39 +0630

    Get HttpResponse and convert it to Map

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,10 +1,30 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+    private final String BASE_URL = "http://localhost:8080";
+    private final String V1_BEER = "/api/v1/beer";
+
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<String> stringResponse =
+                restTemplate.getForEntity(BASE_URL + V1_BEER, String.class);
+        ResponseEntity<Map> mapResponse =
+                restTemplate.getForEntity(BASE_URL + V1_BEER, Map.class);
+        System.out.println(stringResponse);
         return null;
     }
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,16 @@
 package guru.springframework.spring6resttemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class BeerClientImplTest {
+
+    @Autowired
+    BeerClient beerClient;
     @Test
-    void BeerClientImpl_listBeers() {}
+    void BeerClientImpl_listBeers() {
+        beerClient.listBeers();
+    }
 }
